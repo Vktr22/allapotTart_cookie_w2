f1b787d0d49780dc3c980bec9c9520593c1ca4e1
Vktr22 <nds.viktoria@gmail.com>
2025-04-11 08:52:43 +0200

    üdv+ n. látogatás

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+    if(!isset($_COOKIE["visits"])){
+        $visits = 1;
+    }else if(isset($_COOKIE["user"])){
+        $visits = $_COOKIE["visits"]+1;
+    }else {
+        $visits = $_COOKIE["visits"];
+    }
+    setcookie("visits", $visits, time()+35);
     setcookie("user", "Szabó Ágnes", time()+5);
 ?>
 <!DOCTYPE html>
@@ -10,9 +18,10 @@
 </head>
 <body>
     <?php
-        if (isset($_COOKIE["user"]))
+        if (isset($_COOKIE["user"])&&isset($_COOKIE["visits"]))
         {
-            echo "Üdvözöllek kedves ".$_COOKIE["user"]."!";
+            echo "Üdvözöllek ".$_COOKIE["user"]."!";
+            echo "<br>Ez ".$_COOKIE["visits"]." látogatásod.";
         }
         else {
             echo "Üdvözöllek kedves vendég!";
